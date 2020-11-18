ff1b64e743ffcb0147ea525d86f71cf94d038dba
Malte <malte.voss@student.kit.edu>
2020-11-18 13:46:11 +0100

    add file for testing operators

---
+++ 02/material/Test.java
@@ -0,0 +1,13 @@
+class Test {
+    public static void main(String[] args) {
+        System.out.println(9/2);
+        System.out.println(1 + 1/2);
+        System.out.println(5 * 0.5);
+        System.out.println(7 + "Hallo");
+        //System.out.println("Hallo" < "Welt");
+        System.out.println(0.1f > 0.1d);
+        //System.out.println(1.0f == 1.0d);
+        System.out.println(new Test() == "Welt");
+        System.out.println("Hallo" == "hallo");
+    }
+}
